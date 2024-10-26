41815e754e4213e1db405729282357bf9e1523b4
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-26 15:00:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,4 +11,18 @@ import random
 import math
 
 # Inicializa el cliente de Google Maps con tu API key
-gmaps = googlemaps.Client(key='AIzaSyDytpSLPygjIvXWahgD6BABOeMx6VUTQqU')
+gmaps = googlemaps.Client(key='Apikey de google')
+app = Flask(__name__)
+
+# Obtiene las coordenadas GPS de una ciudad
+def obtener_coordenadas(ciudad):
+    geocode_result = gmaps.geocode(ciudad + ", Mexico")
+    if geocode_result:
+        location = geocode_result[0]['geometry']['location']
+        return (location['lat'], location['lng'])
+    else:
+        return None
+
+# Calcula la distancia entre dos puntos usando numpy
+def distancia(p1, p2):
+    return np.linalg.norm(np.array(p1) - np.array(p2))
