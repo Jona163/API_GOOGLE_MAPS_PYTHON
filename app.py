9bc7fd8d5cf5180603624284b8935f6913ca596e
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-26 15:00:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,12 @@
 # Fecha: 25 octubre de 2024 
 # Proyecto: API GOOGLEMAPS
 # GitHub: https://github.com/Jona163
+
+from flask import Flask, request, render_template, jsonify
+import googlemaps
+import numpy as np
+import random
+import math
+
+# Inicializa el cliente de Google Maps con tu API key
+gmaps = googlemaps.Client(key='AIzaSyDytpSLPygjIvXWahgD6BABOeMx6VUTQqU')
